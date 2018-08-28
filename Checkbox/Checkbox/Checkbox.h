dac3c9ab0f8a33f550686a95c1bd201e0cd67f31
viktor hugo <victorHbenitez85@hotmail.com>
2018-08-28 11:51:11 -0500

    Create Checkbox framework

---
+++ Checkbox/Checkbox/Checkbox.h
@@ -0,0 +1,19 @@
+//
+//  Checkbox.h
+//  Checkbox
+//
+//  Created by Victor Hugo Benitez Bosques on 28/08/18.
+//  Copyright © 2018 Victor Hugo Benitez Bosques. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Checkbox.
+FOUNDATION_EXPORT double CheckboxVersionNumber;
+
+//! Project version string for Checkbox.
+FOUNDATION_EXPORT const unsigned char CheckboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Checkbox/PublicHeader.h>
+
+
